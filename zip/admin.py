735410a99224e3db4566b57ecd0cdfd444f9ea03
Sergey Aksenov <telme@list.ru>
2017-08-10 14:24:36 +0300

    Заполнили справочники

--- zip/admin.py
+++ zip/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Zip_types, Zip_locations, Zip_systems
 
-# Register your models here.
+class Zip_types_admin (admin.ModelAdmin):
+    list_display = ('zipType_name', 'zipType_desc')
+
+class Zip_locations_admin (admin.ModelAdmin):
+    list_display = ('Loc_name',)
+
+class Zip_systems_admin (admin.ModelAdmin):
+    list_display = ('Sys_name',)
+
+
+admin.site.register(Zip_types, Zip_types_admin)
+admin.site.register(Zip_locations, Zip_locations_admin)
+admin.site.register(Zip_systems, Zip_systems_admin)
